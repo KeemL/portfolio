76e1b8bdc959ff340a9f71fb029ce72607e0f634
Hakeem Jamal Lingo <hlingo3@gatech.edu>
2024-01-15 03:44:01 -0500

    added assets

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+// var id = null;
+
+// window.addEventListener('scroll', function() {
+//     var div = document.getElementById('logo');
+//     var triggerPoint = div.offsetTop - window.innerHeight; // Adjust the trigger point as needed
+  
+//     if (window.scrollY >= triggerPoint) {
+//       div.style.opacity = 1;
+//       div.style.transform = 'translateY(0)';
+//     }
+//   });
+// const title = document.querySelector("section_text");
+// title.
+
+// const test = []
